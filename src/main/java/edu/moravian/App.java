0ce485f21cf074f459985665355272a50db7f7cc
robertrabinovich <rabinovichr@moravian.edu>
2020-11-10 09:56:09 -0500

    Initial commit

---
+++ src/main/java/edu/moravian/App.java
@@ -0,0 +1,13 @@
+package edu.moravian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
